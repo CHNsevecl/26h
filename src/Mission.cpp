6d243bc3694fc158117bb1c534ed696104ea9b68
CHNsevecl <2593223614@qq.com>
2026-09-12 16:02:25 +0800

    save

--- src/Mission.cpp
+++ src/Mission.cpp
@@ -1,12 +1,20 @@
 #include "Mission.hpp"
+#include <math.h>
 
 // 任务1：每秒打印一次
 void vTask1(void *pvParameters) {
     std::cout << "123" << std::endl;
+    BMI270 imu = *(BMI270 *)pvParameters;
     while (1) {
         if (xSemaphoreTake(xPrintMutex, portMAX_DELAY) == pdTRUE) {
-            ((BMI270 *)pvParameters)->print_state();  // 这里的 printf 被保护
-            // 
+            // imu.print_state();  // 这里的 printf 被保护
+            imu.update_attitude(0.001f); // 这里的 printf 被保护
+            // std::cout << "acc: " << imu.acc_f_[0] << ", " << imu.acc_f_[1] << ", " << imu.acc_f_[2] << std::endl;
+            // std::cout << "gyr: " << imu.gyr_f_[0] << ", " << imu.gyr_f_[1] << ", " << imu.gyr_f_[2] << std::endl;
+            // printf("%.3f,%.3f,%.3f\n", imu.AngleGyrop[0]/180.0f*3.141592, imu.AngleGyrop[1]/180.0f*3.141592, imu.AngleGyrop[2]/180.0f*3.141592);
+            // printf("%.3f,%.3f,%.3f\n", imu.gyr_f_[0], imu.gyr_f_[1], imu.gyr_f_[2]);
+            printf("%.3f,%.3f,%.3f\n",imu.Angle[0]/180.0*3.141592, imu.Angle[1]/180.0*3.141592, imu.Angle[2]/180.0*3.141592);
+        
             xSemaphoreGive(xPrintMutex);              // 释放互斥量
         }
         vTaskDelay(pdMS_TO_TICKS(1)); //不加延时，任务1会一直占用锁，导致2无法正常运行
@@ -26,7 +34,7 @@ void vTask2(void *pvParameters) {
         // vTaskDelay(pdMS_TO_TICKS(500));
 
         std::string str = uart_.uart_echo_receive_string(1000); // 1秒超时
-        std::cout << "Received: " << str << std::endl;
+        // std::cout << "Received: " << str << std::endl;
 
     }
 }
